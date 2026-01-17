b04f9e531b2203a1703e62994c560cb7606e3917
Vivan08 <vivansethi7@gmail.com>
2026-01-17 21:56:34 +0530

    practice questions for java script using push pop

--- JS-Day-3/script.js
+++ JS-Day-3/script.js
@@ -79,21 +79,57 @@ console.log(fruits)
 // }
 
 //answer 3 altenative
-function pallendrome(){
-    let arr=[1,2,3,3,2,1]
-    let arr1 = []
-    let temp = []
-    temp = arr
-    while(arr.length>0){
-    rem=arr.pop()
-    arr1.push(rem);
+// function pallendrome(){
+//     let arr=[1,2,3,3,2,1]
+//     let arr1 = []
+//     let temp = []
+//     temp = arr
+//     while(arr.length>0){
+//     rem=arr.pop()
+//     arr1.push(rem);
+//     }
+//     console.log(arr1)
+//     for(let i =0 ;i<temp.length;i++){
+//         if (temp[i] != arr1[i]){
+//             return "not pallenrome"
+//         }
+//     }
+//     return "pallendrome"
+// }
+// console.log(pallendrome())
+
+
+// answer 4
+// let arr = [1,2,3,4,5,6,7,8]
+// let odd = []
+// let even = []
+// while(arr.length>0){
+//     let rem=arr.pop();
+//     if (rem % 2 ==0){
+//         even.push(rem);
+//     }
+//     else if (rem%2 ==1){
+//         odd.push(rem);
+//     }
+// }
+// console.log("odd numbers are ",odd)
+// console.log("even numbers are ",even)
+//
+
+// answer 5
+let arr=[1,2,3,0,4,5,23,0,0,0,12]
+let arr1=[]
+let zcount = 0
+for(let i = arr.length-1;i>=0;i--){
+    let rem = arr.pop();
+    if (rem == 0){
+        zcount ++;
+    } 
+    else{
+        arr1.push(rem);
     }
-    console.log(arr1)
-    for(let i =0 ;i<temp.length;i++){
-        if (temp[i] != arr1[i]){
-            return "not pallenrome"
-        }
-    }
-    return "pallendrome"
 }
-console.log(pallendrome())
+for(i =0;i<zcount;i++){
+    arr1.push(0);
+}
+console.log(arr1)
