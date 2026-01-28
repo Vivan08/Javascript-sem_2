510427cde5eecf8e7b97dd8102ee3f3605169488
Vivan08 <vivansethi7@gmail.com>
2026-01-28 11:00:26 +0530

    JS-Day 7

--- JS-Day-1/script.js
+++ JS-Day-1/script.js
@@ -1,63 +1,63 @@
-var a = 10;
-console.log(a);
-
-function demo (){
+    var a = 10;
     console.log(a);
-    var b= 20;
-    console.log(b);
-}
-demo();
-// Regular Function
-function sum(){
-let a=10;
-let b=20;
-console.log(a+b)
-}
 
-const sum=(a,b)=>{
-    console.log(a+b);
-}
-sum(5,10)
+    function demo (){
+        console.log(a);
+        var b= 20;
+        console.log(b);
+    }
+    demo();
+    // Regular Function
+    function sum(){
+    let a=10;
+    let b=20;
+    console.log(a+b)
+    }
 
-// Call Back Function 
-function demo(a){
-    return a;
-}
-console.log(demo(5))
+    const sum=(a,b)=>{
+        console.log(a+b);
+    }
+    sum(5,10)
 
-const demo=(a)=>a;
+    // Call Back Function 
+    function demo(a){
+        return a;
+    }
+    console.log(demo(5))
 
-// arrow function is itself a callback function
+    const demo=(a)=>a;
 
-
-// syncronus function
-function sample(){
-    console.log("sample")
-}
-sample()
-
-function demo(){
-    console.log("demo")
-}
-demo()
-//
-
-// syncronus you cannot move to another task before one is completed , asyncronus you can move to another task before one is completed 
+    // arrow function is itself a callback function
 
 
-// asyncronus function
-function sample(callback){
-    console.log("sample");
-    callback()
-}
-function demo(){
-    console.log("demo")
-}
-sample(demo)
-//
+    // syncronus function
+    function sample(){
+        console.log("sample")
+    }
+    sample()
 
-// map
-let numbers = [1,2,3];
-let doubled = numbers.map((x)=>x*2);
-console.log(doubled);
-//
+    function demo(){
+        console.log("demo")
+    }
+    demo()
+    //
+
+    // syncronus you cannot move to another task before one is completed , asyncronus you can move to another task before one is completed 
+
+
+    // asyncronus function
+    function sample(callback){
+        console.log("sample");
+        callback()
+    }
+    function demo(){
+        console.log("demo")
+    }
+    sample(demo)
+    //
+
+    // map
+    let numbers = [1,2,3];
+    let doubled = numbers.map((x)=>x*2);
+    console.log(doubled);
+    //
