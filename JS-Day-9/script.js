4218068c96a4743a8a0a8872729cf6750f750a59
Vivan08 <vivansethi7@gmail.com>
2026-02-03 10:09:14 +0530

    DOM Manupalation

--- JS-Day-9/script.js
+++ JS-Day-9/script.js
@@ -1,12 +1,23 @@
-// query Selector
-const query = document.querySelector("#heading p");
-console.log(query);
+// // query Selector
+// const query = document.querySelector("#heading p");
+// console.log(query);
 
-query.textContent="Hello World from query selector"
+// query.textContent="Hello World from query selector"
 
-// uery selector all
-const queryAll = document.querySelectorAll("p");
-console.log(queryAll);
-for(let i=0; i<queryAll.length; i++){
-    queryAll[i].textContent="jbubcdj uewf yb e" ;
+// // Query selector all
+// const queryAll = document.querySelectorAll("p");
+// console.log(queryAll);
+// for(let i=0; i<queryAll.length; i++){
+//     queryAll[i].textContent="jbubcdj uewf yb e" ;
+// }
+
+const query = document.getElementsByTagName("p");
+for(let i = 0 ; i<query.length;i++){
+    if(i%2==0){
+        query[i].style.color="blue";
+    }
+    else{
+        query[i].style.color="green";
+    }
 }
+query[-1].style.fontweight="bold";
